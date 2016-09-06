df54aa884dbb47bbda2b0d6bff9db90c6e46944e
zhangliuyang1 <945294148@qq.com>
2016-09-06 14:01:59 +0800

    System.getProperty的一些用法

--- src/com/nggirl/test/thinkinjva/IO/InputFromConsole.java
+++ src/com/nggirl/test/thinkinjva/IO/InputFromConsole.java
@@ -11,7 +11,17 @@ import java.util.Scanner;
 public class InputFromConsole {
     public static void main(String[] args) throws IOException {
         byte[] input = new byte[1024];
-        System.in.read(input);
+        //System.in.read(input);
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("file.separator"));
+        System.out.println(System.getProperty("path.separator"));
+        System.out.println(System.getProperty("line.separator"));
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.arch"));
+
 
         System.out.println("your input is:" + new String(input));
     }
